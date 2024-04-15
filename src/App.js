ddd3c79b7a85af17772aef11f8ef912111fc1abc
Verda Muso <gearmobile@gmail.com>
2024-04-15 21:06:59 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from "react";
+
+const Title = {
+  margin: "0 20px",
+};
+
+export default class App extends Component {
+  state = {
+    count: 0,
+    isLoading: false,
+  };
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrementCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button className="Button" onClick={this.incrementCount}>
+          Increment Count +
+        </button>
+        <div style={Title}>Current state: {this.state.count}</div>
+        <button className="Button" onClick={this.decrementCount}>
+          Decrement Count -
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
