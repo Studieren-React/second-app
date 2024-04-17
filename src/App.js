7df28a5929781f63e99b590d72f12a54544469a7
Verda Muso <gearmobile@gmail.com>
2024-04-17 22:12:42 +0300

    interval

--- src/App.js
+++ src/App.js
@@ -2,34 +2,83 @@ import "./App.css";
 
 import { Component } from "react";
 
-const Title = {
-  margin: "0 20px",
-};
-
 export default class App extends Component {
   state = {
     count: 0,
-    isLoading: false,
+    isCalculating: false,
+    time: 1000,
+    item: 'count'
   };
 
-  incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
+  componentDidMount() {
+    this.setState({ count: this.getCount() });
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { count } = this.state;
+    this.saveCount(count);
+  }
+
+  componentWillUnmount() {
+    this.stopCounter(this.interval);
+  }
+
+  start = () => {
+    const { time, isCalculating } = this.state;
+
+    if (!isCalculating) {
+      this.interval = setInterval(() => {
+        this.setState({ count: this.state.count + 1 })
+      }, time);
+    } else {
+      this.stopCounter(this.interval);
+    }
+
+    this.setState({ isCalculating: !this.state.isCalculating });
   };
 
-  decrementCount = () => {
-    this.setState({ count: this.state.count - 1 });
+  reset = () => {
+    this.stopCounter(this.interval);
+    this.setState({ count: 0, isCalculating: false });
   };
+
+  stopCounter = (interval) => {
+    clearInterval(interval);
+  }
+
+  getCount = () => {
+    const count = window.localStorage.getItem(this.state.item);
+
+    if (count) {
+      return Number(count);
+    }
+  }
+
+  saveCount = (count) => {
+    window.localStorage.setItem(this.state.item, count);
+  }
 
   render() {
+    const { isCalculating, count } = this.state;
+
     return (
       <div className="App">
-        <button className="Button" onClick={this.incrementCount}>
-          Increment Count +
-        </button>
-        <div style={Title}>Current state: {this.state.count}</div>
-        <button className="Button" onClick={this.decrementCount}>
-          Decrement Count -
-        </button>
+        <h2 className="Title">react timer</h2>
+        <div className="Counter">Current state: {count}</div>
+        <div className="Wrapper">
+          <button
+              className="Button"
+              onClick={this.start}
+          >
+            {isCalculating ? 'stop' : 'start'}
+          </button>
+          <button
+              className="Button"
+              onClick={this.reset}
+          >
+            reset
+          </button>
+        </div>
       </div>
     );
   }
